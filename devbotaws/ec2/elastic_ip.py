3e7bef43333ad431ac949d06f496d531f473e016
Adam Venturella <aventurella@gmail.com>
2014-05-01 11:56:10 -0700

    adds allocate_elastic_ip_with_conf

--- devbotaws/ec2/elastic_ip.py
+++ devbotaws/ec2/elastic_ip.py
@@ -1,3 +1,4 @@
+from . import utils
 
 
 def allocate_elastic_ip(connection):
@@ -6,3 +7,21 @@ def allocate_elastic_ip(connection):
 
 def release_elastic_ip(connection, ip_address):
     return connection.release_address(ip_address)
+
+
+def allocate_elastic_ip_with_conf(connection, conf):
+    conf_data = conf['elastic_ips']
+
+    # we will replace the existing conf['elastic_ips']
+    # with fully allocated ips if need be.
+    data = {}
+
+    for key, value in utils.process_group(conf_data):
+        if value is None:
+            addy = allocate_elastic_ip(connection)
+            data[key] = addy.public_ip
+        else:
+            data[key] = value
+
+    conf['elastic_ips'] = data
+
