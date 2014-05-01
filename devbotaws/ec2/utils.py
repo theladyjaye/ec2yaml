6a3956b9a57ccd4fe8b3bc3d64b5d7caf1b4f4d6
Adam Venturella <aventurella@gmail.com>
2014-05-01 11:48:10 -0700

    adds group processing

--- devbotaws/ec2/utils.py
+++ devbotaws/ec2/utils.py
@@ -16,3 +16,31 @@ def connection(location='us-east-1'):
 def connection_from_config(conf):
     location = conf['app'].get('location', 'us-east-1')
     return connection(location=location)
+
+
+def key_for_group(item):
+    # basestring only works in python 2
+    if isinstance(item, basestring):
+        return item
+
+    if isinstance(item, dict):
+        return item.keys[0]
+
+
+def value_for_group(item):
+    # basestring only works in python 2
+    if isinstance(item, basestring):
+        return None
+
+    if isinstance(item, dict):
+        return item.values[0]
+
+
+def process_group(items):
+    data = {}
+    for each in items:
+        key = key_for_group(each)
+        value = value_for_group(each)
+        data[key] = value
+
+    return data
