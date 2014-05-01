e887bb64512afe47f87fc3fa505fdae5169b51db
Adam Venturella <aventurella@gmail.com>
2014-05-01 10:59:36 -0700

    updates conneciton util to support location

--- devbotaws/ec2/utils.py
+++ devbotaws/ec2/utils.py
@@ -4,10 +4,15 @@ from boto.ec2.connection import EC2Connection
 __CONNECTION__ = None
 
 
-def connection():
+def connection(location='us-east-1'):
     global __CONNECTION__
 
     if __CONNECTION__ is None:
-        __CONNECTION__ = EC2Connection()
+        __CONNECTION__ = EC2Connection(region_name=location)
 
     return __CONNECTION__
+
+
+def connection_from_config(conf):
+    location = conf['app'].get('location', 'us-east-1')
+    return connection(location=location)
