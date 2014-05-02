1272542cae0b4bb48bec7528dc3b3d795bcf98a8
dinopetrone <dinopetrone@gmail.com>
2014-05-01 17:52:35 -0700

    adding main for hsu

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
+import logging
 from devbotaws import actions
 
 
-if __name__ == "__main__":
-    actions.initialize_with_conf('./tests/resources/sample.conf')
+def main(path):
+    logger = logging.getLogger('devbotaws')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    actions.initialize_with_path(path)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
